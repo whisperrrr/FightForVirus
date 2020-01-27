4bd46730815211d06975afd14f5ed039ff23d4d0
JinHui <jh731448520@163.com>
2020-01-27 15:52:20 +0800

    set basic hubei province map

---
+++ js/index.js
@@ -0,0 +1,90 @@
+var myChart = echarts.init(document.getElementById('main'));
+
+myChart.showLoading();
+
+$.getJSON('./json/statusData.json',function(data){myData = data;});
+
+$.get('./json/hubei.json', function (geoJson) {
+    citysData = parseToCity();
+    myChart.hideLoading();
+
+    echarts.registerMap('湖北', geoJson);
+
+    myChart.setOption(option = {
+        title: {
+            text: '湖北新冠状病毒实时疫情数据(2020.1.27)',
+            subtext: '数据来自丁香医生',
+            sublink: 'http://zh.wikipedia.org/wiki/%E9%A6%99%E6%B8%AF%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83#cite_note-12'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: '{b}<br/>{c}'
+        },
+        toolbox: {
+            show: true,
+            orient: 'vertical',
+            left: 'right',
+            top: 'center',
+            feature: {
+                dataView: {readOnly: false},
+                restore: {},
+                saveAsImage: {}
+            }
+        },
+        visualMap: {
+            min: 800,
+            max: 50000,
+            text: ['High', 'Low'],
+            realtime: false,
+            calculable: true,
+            inRange: {
+                color: ['lightskyblue', 'yellow', 'orangered']
+            }
+        },
+        series: [
+            {
+                name: '湖北新冠状病毒实时疫情数据',
+                type: 'map',
+                mapType: '湖北', // 自定义扩展图表类型
+                label: {
+                    show: true
+                },
+                data: citysData,
+                // 自定义名称映射
+                // nameMap: {
+                //     'Central and Western': '中西区',
+                //     'Eastern': '东区',
+                //     'Islands': '离岛',
+                //     'Kowloon City': '九龙城',
+                //     'Kwai Tsing': '葵青',
+                //     'Kwun Tong': '观塘',
+                //     'North': '北区',
+                //     'Sai Kung': '西贡',
+                //     'Sha Tin': '沙田',
+                //     'Sham Shui Po': '深水埗',
+                //     'Southern': '南区',
+                //     'Tai Po': '大埔',
+                //     'Tsuen Wan': '荃湾',
+                //     'Tuen Mun': '屯门',
+                //     'Wan Chai': '湾仔',
+                //     'Wong Tai Sin': '黄大仙',
+                //     'Yau Tsim Mong': '油尖旺',
+                //     'Yuen Long': '元朗'
+                // }
+            }
+        ]
+    });
+});
+
+function parseToCity() {
+  let cityList = myData[0].cities;
+  let cityListLength = cityList.length;
+  let resultdata = [];
+  for (let i = 0; i < cityListLength; i++) {
+    let city = {name:"",value:""};
+    city.name = cityList[i].cityName.length < 3 ? cityList[i].cityName + "市":cityList[i].cityName;
+    city.value = cityList[i].confirmedCount;
+    resultdata.push(city);
+  }
+  return resultdata;
+}
