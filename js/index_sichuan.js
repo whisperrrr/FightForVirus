18d58fa0fe7b2e48d67055ff8cf98fe1f0a3db5a
ChuanchaoXu <39235169+ChuanchaoXu@users.noreply.github.com>
2020-01-29 17:46:42 +0800

    Update index_sichuan.js

--- js/index_sichuan.js
+++ js/index_sichuan.js
@@ -12,7 +12,7 @@ $.get('./json/sichuan.json', function (geoJson) {
 
     myChart.setOption(option = {
         title: {
-            text: '四川省新冠状病毒实时疫情数据(2020.1.29 10:40(北京时间))',
+            text: '四川省新冠状病毒实时疫情数据(2020.1.29 17:44(北京时间))',
             subtext: '数据来自丁香医生',
             sublink: 'https://3g.dxy.cn/newh5/view/pneumonia_peopleapp?from=singlemessage&isappinstalled=0'
         },
