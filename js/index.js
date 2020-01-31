deba82a21862b270329230533d47fa16b2976241
JinHui <jh731448520@163.com>
2020-01-31 16:12:19 +0800

    updata data 2020-01-31 15:48

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ $.get('./json/hubei.json', function (geoJson) {
 
     myChart.setOption(option = {
         title: {
-            text: '湖北省新冠状病毒实时疫情数据(2020-01-31 11:26(北京时间))',
+            text: '湖北省新冠状病毒实时疫情数据(2020-01-31 15:48 (北京时间))',
             subtext: '数据来自丁香医生',
             sublink: 'https://3g.dxy.cn/newh5/view/pneumonia_peopleapp?from=singlemessage&isappinstalled=0'
         },
