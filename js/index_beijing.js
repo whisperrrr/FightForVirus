217ea9b183bb8044f5689b8aefb78992b715d587
JinHui <jh731448520@163.com>
2020-02-03 20:24:46 +0800

    update data 2020-02-03 19:28

--- js/index_beijing.js
+++ js/index_beijing.js
@@ -12,7 +12,7 @@ $.get('./json/beijing.json', function (geoJson) {
 
     myChart.setOption(option = {
         title: {
-            text: '北京市新冠状病毒实时疫情数据(2020-02-02 15:41(北京时间))',
+            text: '北京市新冠状病毒实时疫情数据(2020-02-03 19:28(北京时间))',
             subtext: '数据来自丁香医生',
             sublink: 'https://3g.dxy.cn/newh5/view/pneumonia_peopleapp?from=singlemessage&isappinstalled=0'
         },
