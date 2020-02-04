412d52fd7eaa24da350786da1cf21ec2f474ae0b
ChuanchaoXu <39235169+ChuanchaoXu@users.noreply.github.com>
2020-02-04 09:31:15 +0800

    Update index_otherProvinces.js

--- js/index_otherProvinces.js
+++ js/index_otherProvinces.js
@@ -12,7 +12,7 @@ $.get('./json/henan.json', function (geoJson) {
 
     myChart.setOption(option = {
         title: {
-            text: '河南省新冠状病毒实时疫情数据(2020-02-03 19:28(北京时间))',
+            text: '河南省新冠状病毒实时疫情数据(2020-02-04 09:28(北京时间))',
             subtext: '数据来自丁香医生',
             sublink: 'https://3g.dxy.cn/newh5/view/pneumonia_peopleapp?from=singlemessage&isappinstalled=0'
         },
